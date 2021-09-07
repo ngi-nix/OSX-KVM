af99702855eb803ecf70b370cbdd15928574296b
Mihai Fufezan <fufexan@pm.me>
2021-09-07 11:57:32 +0300

    add options, headless args
    the readonly=on flag makes the VM work on AMD machines

--- flake.nix
+++ flake.nix
@@ -22,35 +22,43 @@
         '';
       };
 
-      runMacOS = final.writeShellScriptBin "runMacOS" ''
-        cp ${osx-kvm}/firmware/OVMF_VARS-1024x768.fd OVMF_VARS.fd
-        cp ${osx-kvm}/ESP.qcow2 .
-        chmod a+w OVMF_VARS.fd
-        chmod a+w ESP.qcow2
+      runMacOS =
+        let
+          inherit (nixpkgs.lib) optionalString;
+          cores = 2;
+          ram = "6G";
+          headless = false;
+        in
+        final.writeShellScriptBin "runMacOS" ''
+          cp ${osx-kvm}/firmware/OVMF_VARS-1024x768.fd OVMF_VARS.fd
+          cp ${osx-kvm}/ESP.qcow2 .
+          chmod a+w OVMF_VARS.fd
+          chmod a+w ESP.qcow2
 
-        ${final.qemu}/bin/qemu-system-x86_64 \
-           -enable-kvm \
-           -m 6G \
-           -machine q35,accel=kvm \
-           -smp 4,cores=2 \
-           -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
-           -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
-           -smbios type=2 \
-           -drive if=pflash,format=raw,readonly,file="${osx-kvm}/firmware/OVMF_CODE.fd" \
-           -drive if=pflash,format=raw,file="OVMF_VARS.fd" \
-           -vga qxl \
-           -device ich9-intel-hda -device hda-output \
-           -usb -device usb-kbd -device usb-mouse \
-           -netdev user,id=net0 \
-           -device virtio-net,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
-           -device ich9-ahci,id=sata \
-           -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
-           -device ide-hd,bus=sata.2,drive=ESP \
-           -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
-           -device ide-hd,bus=sata.3,drive=InstallMedia \
-           -drive id=SystemDisk,if=none,file=disk0.qcow2 \
-           -device virtio-blk,drive=SystemDisk \
-      '';
+          ${final.qemu}/bin/qemu-system-x86_64 \
+             -enable-kvm \
+             -m ${ram} \
+             -machine q35,accel=kvm \
+             -smp ${toString (cores * 2)},cores=${toString cores} \
+             -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
+             -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
+             -smbios type=2 \
+             -drive if=pflash,format=raw,readonly=on,file="${osx-kvm}/firmware/OVMF_CODE.fd" \
+             -drive if=pflash,format=raw,file="OVMF_VARS.fd" \
+             -vga qxl \
+             -device ich9-intel-hda -device hda-output \
+             -usb -device usb-kbd -device usb-mouse \
+             -netdev user,id=net0 \
+             -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
+             -device ich9-ahci,id=sata \
+             -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
+             -device ide-hd,bus=sata.2,drive=ESP \
+             -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
+             -device ide-hd,bus=sata.3,drive=InstallMedia \
+             -drive id=SystemDisk,if=none,file=disk0.qcow2 \
+             -device virtio-blk,drive=SystemDisk \
+             ${optionalString headless "-nographic -vnc :0 -k en-us"} \
+        '';
     };
 
   } // flake-utils.lib.eachDefaultSystem (system:
