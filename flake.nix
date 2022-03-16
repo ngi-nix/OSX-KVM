b2875628cbfc5adf95071abf01cc3f63f2c2a561
ostylk <ostylk@googlemail.com>
2022-03-16 18:02:50 +0100

    moved start script out of flake.nix

--- flake.nix
+++ flake.nix
@@ -14,56 +14,8 @@
 
         fetchMacOS = pkgs.callPackage ./packages/fetchMacOS.nix { inherit osx-kvm; };
 
-        start =
-          pkgs.writeShellScriptBin "start" ''
-            set -e
-
-            if [ ! -e disk0.qcow2 ];then
-              ${self.packages.${system}.init}/bin/init
-            fi
-
-            cp ${osx-kvm}/firmware/OVMF_VARS-1024x768.fd OVMF_VARS.fd
-            cp ${osx-kvm}/ESP.qcow2 .
-            chmod a+w OVMF_VARS.fd
-            chmod a+w ESP.qcow2
-
-            export ramSize=$(jq -r '.ramSize' settings.json)
-            export cores=$(jq -r '.cores' settings.json)
-            export headless=$(jq -r '.headless' settings.json)
-
-            if [ "$headless" == "true" ]; then
-              headless="-nographic -vnc :0 -k en-us"
-            else
-              headless=""
-            fi
-
-            ${pkgs.qemu}/bin/qemu-system-x86_64 \
-              -enable-kvm \
-              -m $ramSize \
-              -machine q35,accel=kvm \
-              -smp $(( $cores * 2 )),cores=$cores \
-              -cpu Penryn,vendor=GenuineIntel,kvm=on,+sse3,+sse4.2,+aes,+xsave,+avx,+xsaveopt,+xsavec,+xgetbv1,+avx2,+bmi2,+smep,+bmi1,+fma,+movbe,+invtsc \
-              -device isa-applesmc,osk="ourhardworkbythesewordsguardedpleasedontsteal(c)AppleComputerInc" \
-              -smbios type=2 \
-              -drive if=pflash,format=raw,readonly=on,file="${osx-kvm}/firmware/OVMF_CODE.fd" \
-              -drive if=pflash,format=raw,file="OVMF_VARS.fd" \
-              -vga qxl \
-              -device ich9-intel-hda -device hda-output \
-              -usb -device usb-kbd -device usb-mouse \
-              -netdev user,id=net0 \
-              -device e1000-82545em,netdev=net0,id=net0,mac=52:54:00:c9:18:27 \
-              -device ich9-ahci,id=sata \
-              -drive id=ESP,if=none,format=qcow2,file=ESP.qcow2 \
-              -device ide-hd,bus=sata.2,drive=ESP \
-              -drive id=InstallMedia,format=raw,if=none,file=BaseSystem.img \
-              -device ide-hd,bus=sata.3,drive=InstallMedia \
-              -drive id=SystemDisk,if=none,file=disk0.qcow2 \
-              -device virtio-blk,drive=SystemDisk \
-              $headless \
-          '';
-
         init = pkgs.callPackage ./packages/init.nix { inherit fetchMacOS; };
-
+        start = pkgs.callPackage ./packages/start.nix { inherit init osx-kvm; };
       in
       {
         packages = { inherit start init; };
---
+++ packages/start.nix
@@ -0,0 +1,26 @@
+{ stdenv
+, init
+, jq
+, lib
+, makeWrapper
+, osx-kvm
+, qemu
+}:
+
+stdenv.mkDerivation {
+  name = "start";
+  src = ./start.sh;
+
+  dontUnpack = true;
+
+  nativeBuildInputs = [ makeWrapper ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/start
+    chmod a+x $out/bin/start
+    wrapProgram $out/bin/start \
+      --prefix PATH : ${lib.makeBinPath [ init jq qemu ]} \
+      --set OSX ${osx-kvm}
+  '';
+}
